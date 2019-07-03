f5bc6100e215dd96b6aceab05b4729a277574142
Nedko Ivanov <ivanovnedko@gmail.com>
2019-07-03 14:32:11 +0300

    additions

--- views/booking-form.php
+++ views/booking-form.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="/rentals/assets/stylesheet.css">
+</head>
 <body>
 
+<div class="container">
 <h2>Booking Form</h2>
 
 <form action="/action_page.php">
-  First name:<br>
-  <input type="text" name="firstname" value="Mickey">
+  Rental Start:<br>
+  <input type="date" name="booked_from" value="">
   <br>
-  Last name:<br>
-  <input type="text" name="lastname" value="Mouse">
-  <br><br>
-  <input type="submit" value="Submit">
+  Rental End:<br>
+  <input type="date" name="booked_to" value="">
+  <br>
+  Car:<br>
+  <select name="cars">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+  </select>
+  <br>
+  <input type="submit" value="Search">
 </form> 
 
-
+</div>
 
 <button onclick="goBack()">Go Back</button>
 
