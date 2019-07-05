df77d79d29d8b9fa625f86f8bb39f5c4fe18fd94
Nedko Ivanov <ivanovnedko@gmail.com>
2019-07-05 15:43:01 +0300

    add sql dumps

--- SQL/rentals-without-create-db.sql
+++ SQL/rentals-without-create-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 04, 2019 at 05:00 PM
+-- Generation Time: Jul 05, 2019 at 02:41 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `booking` (
   `id` int(10) UNSIGNED NOT NULL,
-  `car_id` int(11) NOT NULL,
+  `car_id` int(11) UNSIGNED NOT NULL,
   `booked_from` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `booked_to` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `total` decimal(7,2) NOT NULL
@@ -41,17 +41,18 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`id`, `car_id`, `booked_from`, `booked_to`, `total`) VALUES
-(1, 1, '2019-07-03 21:00:00', '2019-07-04 21:00:00', '40.00'),
-(2, 1, '2019-07-07 21:00:00', '2019-07-11 21:00:00', '90.00'),
-(3, 1, '2019-07-20 21:00:00', '2019-07-25 21:00:00', '90.00'),
-(4, 2, '2019-07-03 21:00:00', '2019-07-04 21:00:00', '40.00'),
-(5, 3, '2019-07-03 21:00:00', '2019-07-10 21:00:00', '108.00'),
-(6, 4, '2019-07-15 21:00:00', '2019-07-16 21:00:00', '40.00'),
-(7, 4, '2019-07-03 21:00:00', '2019-07-08 21:00:00', '72.00'),
-(8, 2, '2019-07-31 21:00:00', '2019-08-13 21:00:00', '160.00'),
-(9, 1, '2019-08-31 21:00:00', '2019-09-25 21:00:00', '304.00'),
-(10, 3, '2019-08-19 21:00:00', '2019-08-21 21:00:00', '60.00'),
-(11, 5, '2019-07-03 21:00:00', '2019-07-04 21:00:00', '40.00');
+(14, 1, '2019-07-04 21:00:00', '2019-07-18 21:00:00', '176.00'),
+(15, 19, '2019-07-23 21:00:00', '2019-07-29 21:00:00', '90.00'),
+(16, 16, '2019-07-09 21:00:00', '2019-07-24 21:00:00', '192.00'),
+(17, 18, '2019-07-25 21:00:00', '2019-09-24 21:00:00', '704.00'),
+(18, 4, '2019-07-04 21:00:00', '2019-07-23 21:00:00', '224.00'),
+(19, 3, '2019-08-31 21:00:00', '2019-09-17 21:00:00', '208.00'),
+(20, 15, '2019-07-16 21:00:00', '2019-08-30 21:00:00', '528.00'),
+(21, 2, '2019-07-09 21:00:00', '2019-08-19 21:00:00', '480.00'),
+(22, 24, '2019-07-24 21:00:00', '2019-11-19 22:00:00', '1360.00'),
+(23, 18, '2019-07-18 21:00:00', '2019-07-24 21:00:00', '90.00'),
+(24, 31, '2019-07-04 21:00:00', '2019-07-24 21:00:00', '240.00'),
+(25, 14, '2019-07-04 21:00:00', '2019-07-07 21:00:00', '40.00');
 
 -- --------------------------------------------------------
 
@@ -99,7 +100,30 @@ INSERT INTO `car` (`id`, `model_id`, `engine_id`, `manufacture_year`) VALUES
 (3, 9, 3, 1995),
 (4, 13, 7, 2012),
 (5, 2, 6, 2018),
-(6, 2, 5, 2001);
+(6, 2, 5, 2001),
+(7, 7, 3, 2011),
+(8, 9, 8, 2005),
+(9, 12, 3, 2011),
+(10, 15, 1, 2005),
+(11, 15, 1, 2005),
+(12, 4, 8, 2005),
+(13, 10, 8, 2005),
+(14, 5, 6, 2011),
+(15, 7, 8, 2005),
+(16, 7, 8, 2005),
+(17, 7, 8, 2005),
+(18, 2, 6, 2011),
+(19, 2, 6, 2011),
+(20, 2, 6, 2011),
+(21, 11, 7, 2005),
+(24, 19, 1, 2019),
+(26, 18, 6, 2006),
+(27, 11, 7, 2011),
+(28, 19, 5, 2011),
+(29, 16, 7, 2019),
+(30, 12, 1, 2005),
+(31, 22, 8, 2019),
+(32, 11, 6, 2019);
 
 -- --------------------------------------------------------
 
@@ -132,7 +156,72 @@ INSERT INTO `car_feature` (`car_id`, `feature_id`) VALUES
 (4, 5),
 (4, 11),
 (4, 4),
-(4, 7);
+(4, 7),
+(8, 1),
+(8, 3),
+(8, 6),
+(8, 9),
+(10, 1),
+(10, 2),
+(10, 3),
+(10, 4),
+(10, 5),
+(10, 6),
+(10, 7),
+(11, 1),
+(11, 2),
+(11, 3),
+(11, 4),
+(11, 5),
+(11, 6),
+(11, 7),
+(12, 1),
+(12, 2),
+(12, 3),
+(12, 4),
+(12, 5),
+(12, 6),
+(12, 7),
+(12, 8),
+(12, 9),
+(12, 10),
+(12, 11),
+(12, 12),
+(13, 1),
+(13, 5),
+(13, 6),
+(14, 1),
+(14, 2),
+(14, 4),
+(14, 8),
+(15, 5),
+(15, 9),
+(16, 5),
+(16, 9),
+(17, 5),
+(17, 9),
+(24, 1),
+(24, 5),
+(24, 7),
+(24, 10),
+(26, 3),
+(26, 6),
+(27, 4),
+(27, 7),
+(27, 10),
+(28, 4),
+(28, 7),
+(28, 10),
+(29, 1),
+(29, 9),
+(30, 1),
+(30, 12),
+(31, 2),
+(31, 7),
+(32, 1),
+(32, 2),
+(32, 3),
+(32, 4);
 
 -- --------------------------------------------------------
 
@@ -151,14 +240,14 @@ CREATE TABLE `engine` (
 --
 
 INSERT INTO `engine` (`id`, `fuel_type`, `transmission`) VALUES
-(1, 'Petrol', 'Manual gearbox'),
-(2, 'Petrol', 'Automatic transmission'),
 (3, 'Diesel', 'Manual gearbox'),
-(4, 'Petrol', 'Semi-automatic'),
 (5, 'Electric', 'Manual gearbox'),
 (6, 'Electric', 'Semi-automatic'),
+(8, 'Hydrogen', 'Automatic transmission'),
 (7, 'Natural Gas', 'Manual gearbox'),
-(8, 'Hydrogen', 'Automatic transmission');
+(1, 'Petrol', 'Manual gearbox'),
+(4, 'Petrol', 'Semi-automatic'),
+(2, 'Petrol', 'Automatic transmission');
 
 -- --------------------------------------------------------
 
@@ -261,7 +350,10 @@ INSERT INTO `price_plan` (`id`, `rent_interval_start`, `rent_interval_end`, `pri
 -- Indexes for table `booking`
 --
 ALTER TABLE `booking`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `booked_from` (`booked_from`),
+  ADD KEY `booked_to` (`booked_to`),
+  ADD KEY `booking_car_id` (`car_id`);
 
 --
 -- Indexes for table `brand`
@@ -288,7 +380,8 @@ ALTER TABLE `car_feature`
 -- Indexes for table `engine`
 --
 ALTER TABLE `engine`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `fuel_type` (`fuel_type`,`transmission`);
 
 --
 -- Indexes for table `feature`
@@ -317,7 +410,7 @@ ALTER TABLE `price_plan`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `brand`
@@ -329,13 +422,13 @@ ALTER TABLE `brand`
 -- AUTO_INCREMENT for table `car`
 --
 ALTER TABLE `car`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `engine`
 --
 ALTER TABLE `engine`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `feature`
@@ -358,6 +451,12 @@ ALTER TABLE `price_plan`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_car_id` FOREIGN KEY (`car_id`) REFERENCES `car` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `car`
